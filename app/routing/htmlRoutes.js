09161268156db5fb90adc96bf678942850900635
Denton Kerr <dentonkerr@lawn-128-61-30-68.lawn.gatech.edu>
2018-02-24 12:01:03 -0500

    fixes htmlRoute function

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,19 +1,17 @@
 var path = require("path");
 
-function getHTML(app) {
+module.exports = function (app) {
 
-app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/tables.html"))
-});
+    app.get("/tables", function (req, res) {
+        res.sendFile(path.join(__dirname, "app/public/tables.html"))
+    });
 
-app.get("/reservation", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/reservation.html"))
-});
+    app.get("/reservation", function (req, res) {
+        res.sendFile(path.join(__dirname, "app/public/reservation.html"))
+    });
 
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/index.html"))
-});
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "app/public/index.html"))
+    });
 
-};
-
-module.exports = getHTML;
+}
