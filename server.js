05c0f268cfa5641b747ffc5d92a64631ace50e46
Elaina Swann <elaina@odecisions.com>
2018-02-24 11:17:30 -0500

    Adds express.static.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
