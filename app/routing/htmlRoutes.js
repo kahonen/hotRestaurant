a7141fe8205a5bce9ea702a0cb8c128da8ab8462
Elaina Swann <elaina@odecisions.com>
2018-02-24 13:35:56 -0500

    Bug fixes with url for home html.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/reservation.html"))
     });
 
-    app.get("*", function (req, res) {
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"))
     });
 
